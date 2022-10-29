7cdca3b5863bac2493028038814ce926ed8381db
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-10-29 18:04:43 +0530

    completed

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x3d3ABeCaeD45EFEf13Ed86F678B2bbBe76C4372C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://shimmering-zuccutto-dcdf37.netlify.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
